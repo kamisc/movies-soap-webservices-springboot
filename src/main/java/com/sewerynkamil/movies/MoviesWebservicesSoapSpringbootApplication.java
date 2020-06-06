21c9de6e69484d52196f65039d0d1ba926a43b63
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-06 11:12:34 +0200

    initial commit

---
+++ src/main/java/com/sewerynkamil/movies/MoviesWebservicesSoapSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesWebservicesSoapSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesWebservicesSoapSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sewerynkamil/movies/MoviesWebservicesSoapSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesWebservicesSoapSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
