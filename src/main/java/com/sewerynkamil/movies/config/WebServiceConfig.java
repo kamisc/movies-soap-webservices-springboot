fe401b6be123681efae5aab7fa2234c0aee1440c
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-06-09 22:59:11 +0200

    Updating WebServiceConfig - adding exceptionResolver() method

--- src/main/java/com/sewerynkamil/movies/config/WebServiceConfig.java
+++ src/main/java/com/sewerynkamil/movies/config/WebServiceConfig.java
@@ -1,5 +1,7 @@
 package com.sewerynkamil.movies.config;
 
+import com.sewerynkamil.movies.exception.DetailSoapFaultDefinitionExceptionResolver;
+import com.sewerynkamil.movies.exception.ServiceFaultException;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +9,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.soap.server.endpoint.SoapFaultDefinition;
+import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.Properties;
 
 @EnableWs
 @Configuration
@@ -36,5 +42,21 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema moviesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("movies.xsd"));
+    }
+
+    @Bean
+    public SoapFaultMappingExceptionResolver exceptionResolver() {
+        SoapFaultMappingExceptionResolver exceptionResolver = new DetailSoapFaultDefinitionExceptionResolver();
+
+        SoapFaultDefinition faultDefinition = new SoapFaultDefinition();
+        faultDefinition.setFaultCode(SoapFaultDefinition.SERVER);
+        exceptionResolver.setDefaultFault(faultDefinition);
+
+        Properties errorMappings = new Properties();
+        errorMappings.setProperty(Exception.class.getName(), SoapFaultDefinition.SERVER.toString());
+        errorMappings.setProperty(ServiceFaultException.class.getName(), SoapFaultDefinition.SERVER.toString());
+        exceptionResolver.setExceptionMappings(errorMappings);
+        exceptionResolver.setOrder(1);
+        return exceptionResolver;
     }
 }
